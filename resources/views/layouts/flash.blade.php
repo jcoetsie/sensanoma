477fc86743bfcf60e8ab58f9a99d149d2b17fd16
Jonathan V <hxn@live.fr>
2017-11-12 15:30:43 +0100

    Change flash message style

--- resources/views/layouts/flash.blade.php
+++ resources/views/layouts/flash.blade.php
@@ -1,26 +1,31 @@
 @if (session()->has('success'))
-    <div class="alert alert-success">
+    <div class="alert alert-success alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4><i class="icon fa fa-check"></i> Success!</h4>
         {{ session('success') }}
     </div>
 @endif
 
 @if (session()->has('info'))
-    <div class="alert alert-info">
+    <div class="alert alert-info alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4><i class="icon fa fa-info"></i> Info!</h4>
         {{ session('info') }}
     </div>
 @endif
 
 @if (session()->has('warning'))
-    <div class="alert alert-warning alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
+    <div class="alert alert-warning alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4><i class="icon fa fa-warning"></i> Warning!</h4>
         {{ session('warning') }}
     </div>
 @endif
 
 @if (session()->has('danger'))
-    <div class="alert alert-danger">
+    <div class="alert alert-danger alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4><i class="icon fa fa-ban"></i> Error!</h4>
         {{ session('danger') }}
     </div>
 @endif
