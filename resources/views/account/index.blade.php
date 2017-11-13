2f31b36e0c909c23e426f9ecc06a10ed29a3c66c
DYSurroundings <joao_mnstrkhaos@hotmail.com>
2017-11-13 13:29:45 +0100

    Index page for accounts created, added account options

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -1,12 +1,43 @@
 @extends('adminlte::page')
 
 @section('title')
+  Sensanoma
+@stop
 
 @section('content_header')
     <h1>Account settings</h1>
 @stop
 
 @section('content')
-    <p>Settings will be coming soon</p>
-    <a href="{{ route('account.edit', $account) }}">Edit account</a>
+
+    <div class="col-md-12">
+        <div class="box">
+            <div class="box-body no-padding">
+                <table class="table">
+                    <tr>
+                        <th style="width: 80%;">Name</th>
+                        <th>Options</th>
+                    </tr>
+
+                    <tr>
+                        <td>{{ $account->name }}</td>
+                            <td>
+                                <a href="{{ route('account.edit', $account) }}">
+                                    <button type="button" class="btn btn-block btn-info">Update</button>
+                                </a>
+                            </td>
+                            <td>
+                                <a href="{{ route('account.destroy', $account) }}">
+                                    <button type="button" class="btn btn-block btn-danger">Delete</button>
+                                </a>
+                            </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+        <a href="{{ route('account.create', $account) }}">
+            <button type="button" class="btn btn-block btn-primary">Create an account</button>
+        </a>
+
 @stop
