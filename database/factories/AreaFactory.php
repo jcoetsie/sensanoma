68d583fee61cd9ce9b4ac6821b6142dd92a596d6
Jonathan V <hxn@live.fr>
2017-11-15 21:32:58 +0100

    Add all factories

--- database/factories/AreaFactory.php
+++ database/factories/AreaFactory.php
@@ -4,8 +4,10 @@ use Faker\Generator as Faker;
 
 /* @var Illuminate\Database\Eloquent\Factory $factory */
 
-$factory->define(App\Area::class, function (Faker $faker) {
+$factory->define(App\Models\Area::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->city,
+        'address' => $faker->streetAddress,
+        'coordinates' => "[" .json_encode(['lat' => $faker->latitude, 'lng' => $faker->longitude]). "]",
     ];
 });
--- database/factories/SensorNodeFactory.php
+++ database/factories/SensorNodeFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\SensorNode::class, function (Faker $faker) {
     return [
-        //
+        'name' => 'b000',
+        'type' => 'B-sprout'
     ];
 });
