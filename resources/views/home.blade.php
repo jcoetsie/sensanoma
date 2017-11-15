4e60d461625d0059ad68ec9aebf9ce2fac78bb1f
DYSurroundings <joao_mnstrkhaos@hotmail.com>
2017-11-15 15:21:13 +0100

    Update home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,48 +12,112 @@
 @section('content')
     @include('layouts.flash')
     <div class="col-md-12">
-        <div class="small-box bg-green">
-            <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
+        <div class="col-lg-6 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3>12<sup style="font-size: 20px">°</sup></h3>
 
-                <p>Humidity Rate</p>
+                    <p>Air temperature</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-thermometer-2"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
-            <div class="icon">
-                <i class="ion ion-waterdrop"></i>
-            </div>
-            <a href="#" class="small-box-footer">
-                More info <i class="fa fa-arrow-circle-right"></i>
-            </a>
         </div>
 
-        <div class="small-box bg-red">
-            <div class="inner">
-                <h3>15<sup style="font-size: 20px">°</sup></h3>
+        <div class="col-lg-6 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3>15<sup style="font-size: 20px">%</sup></h3>
 
-                <p>Soil Temp</p>
+                    <p>Air humidity</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-waterdrop"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
-            <div class="icon">
-                <i class="ion ion-stats-bars"></i>
-            </div>
-            <a href="#" class="small-box-footer">
-                More info <i class="fa fa-arrow-circle-right"></i>
-            </a>
         </div>
 
-        <div class="small-box bg-blue">
-            <div class="inner">
-                <h3>1.762<sup style="font-size: 20px">Lumen</sup></h3>
+        <div class="col-lg-6 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>18<sup style="font-size: 20px">°</sup></h3>
 
-                <p>Solar energy</p>
+                    <p>Soil temperature</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-thermometer-2"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
-            <div class="icon">
-                <i class="ion ion-android-sunny"></i>
+        </div>
+
+        <div class="col-lg-6 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>31<sup style="font-size: 20px">%</sup></h3>
+
+                    <p>Soil humidity</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-waterdrop"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
-            <a href="#" class="small-box-footer">
-                More info <i class="fa fa-arrow-circle-right"></i>
-            </a>
+        </div>
+
+        <div class="col-lg-12 col-xs-12">
+            <!-- small box -->
+            <div class="small-box bg-orange">
+                <div class="inner">
+                    <h3>6<sup style="font-size: 20px">lm</sup></h3>
+
+                    <p>Lumen</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-android-sunny"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-12 col-xs-12">
+            <!-- small box -->
+            <div class="small-box bg-yellow">
+                <div class="inner">
+                    <h3>31<sup style="font-size: 20px">v</sup></h3>
+
+                    <p>Voltage</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-bolt"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-12 col-xs-12">
+            <!-- small box -->
+            <div class="small-box bg-red">
+                <div class="inner">
+                    <h3>31<sup style="font-size: 20px">°</sup></h3>
+
+                    <p>Internal temperature</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-thermometer-2"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
         </div>
     </div>
+
 
 @stop
 
