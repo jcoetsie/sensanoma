24553a8ab6d42f0bc49591be6ab264923266c3a3
hxn-dev <hxn@live.fr>
2017-11-15 11:33:23 +0100

    Init AddSensorNodeType

---
+++ app/Http/Controllers/SensorNodeTypeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SensorNodeType;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SensorNodeTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SensorNodeType  $sensorNodeType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SensorNodeType $sensorNodeType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SensorNodeType  $sensorNodeType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SensorNodeType $sensorNodeType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SensorNodeType  $sensorNodeType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SensorNodeType $sensorNodeType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SensorNodeType  $sensorNodeType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SensorNodeType $sensorNodeType)
+    {
+        //
+    }
+}
