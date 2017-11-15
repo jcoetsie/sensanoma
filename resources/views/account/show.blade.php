6f35330824d08bb21720dc22eea00135bd279d9a
DYSurroundings <joao_mnstrkhaos@hotmail.com>
2017-11-15 14:00:20 +0100

    update account style in edit, index, show close section title in home.blade

--- resources/views/account/show.blade.php
+++ resources/views/account/show.blade.php
@@ -5,34 +5,37 @@
     <h1>Account</h1>
 @stop
 
+@section('css')
+    <link rel="stylesheet" href="{{ URL::asset('css/custom.css') }}" />
+@stop
+
 @section('content')
 
-    <div class="col-md-12">
-        <div class="box">
-            <div class="box-body no-padding">
-                <table class="table">
-                    <tr>
-                        <th style="width: 80%">Name</th>
-                        <th>Action</th>
-                    </tr>
-                    <tr>
-                        <td style="width: 80%">{{ $account->name }}</td>
-                        <td>
-                            <a href="{{ route('account.edit', $account) }}">
-                                <button type="button" class="btn btn-block btn-info">Update</button>
-                            </a>
-                        </td>
-                        <td>
-                            {{ html()->form('DELETE', route('account.destroy', $account))->open() }}
+    <table class="table borderless">
+        <tr>
+            <th style="width: 80%">Name</th>
+        </tr>
+        <tr>
+            <td style="width: 80%">{{ $account->name }}</td>
+            <td class="pull-right">
 
-                            {{ html()->submit('Delete')->class('btn btn-danger') }}
+                {{ html()->form('DELETE', route('account.destroy', $account->id))->open() }}
 
-                            {{ html()->form()->close() }}
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
+                {{ html()->submit('Delete')->class('btn custom btn-danger') }}
+
+                {{ html()->form()->close() }}
+
+            </td>
+
+            <td class="pull-right">
+
+                {{ html()->form('GET', route('account.edit', $account->id))->open() }}
+
+                {{ html()->submit('Edit')->class('btn custom btn-info') }}
+
+                {{ html()->form()->close() }}
+            </td>
+        </tr>
+    </table>
 
 @stop
