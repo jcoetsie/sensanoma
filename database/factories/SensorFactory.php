d63f745eadd73a3d2115e505081692a589e8434a
hxn-dev <hxn@live.fr>
2017-11-15 17:59:24 +0100

    Remove SensorNodeType and Sensor

--- database/factories/SensorFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Models\Sensor::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,5 @@ Route::resource('account', 'AccountController');
 Route::resource('area', 'AreaController');
 Route::resource('zone', 'ZoneController');
 Route::resource('sensor_node', 'SensorNodeController');
-Route::resource('sensor_node_type', 'SensorNodeTypeController');
 
 Auth::routes();
