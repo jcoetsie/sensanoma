2a9d990a538adf1a74d8d97d1ce3682be74cd2e1
Jonathan V <hxn@live.fr>
2017-11-18 19:18:51 +0100

    Add Zone tests

---
+++ tests/Feature/ZoneControllerAdminTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Area;
+use App\Models\Role;
+use App\Models\SensorNode;
+use App\Models\User;
+use App\Models\Zone;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ZoneControllerAdminTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public $userAdmin;
+    public $userAdmin2;
+    public $zone;
+    public $sensorNode;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // Create Admin role
+        factory(Role::class)->create();
+
+        $this->userAdmin = factory(User::class)->create();
+        $this->userAdmin->attachRole('admin');
+
+        $this->area = factory(Area::class)->create(['account_id' => $this->userAdmin->account->id]);
+        $this->zone = factory(Zone::class)->create(['area_id' => $this->area->id]);
+        $this->sensorNode = factory(SensorNode::class)->create(['zone_id' => $this->zone->id]);
+    }
+
+    /** @test **/
+    public function it_should_return_a_collection_of_zones()
+    {
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->zone->sensorNodes()->get());
+    }
+
+    /** @test **/
+    public function a_user_with_admin_role_can_index_his_zone()
+    {
+        $response = $this->actingAs($this->userAdmin)
+            ->get(route('zone.index', $this->zone));
+
+        $response->assertStatus(200);
+    }
+
+    /** @test **/
+    public function a_user_with_admin_role_can_show_his_zone()
+    {
+        $response = $this->actingAs($this->userAdmin)
+            ->get(route('zone.show', $this->zone));
+
+        $response->assertStatus(200);
+    }
+
+    /** @test **/
+    public function a_user_with_admin_role_can_create_his_area()
+    {
+        $response = $this->actingAs($this->userAdmin)
+            ->get(route('zone.create'));
+
+        $response->assertStatus(200);
+    }
+
+    /** @test **/
+    public function a_user_with_admin_role_can_store_his_area()
+    {
+        $response = $this->actingAs($this->userAdmin)
+            ->post(route('zone.store', $this->zone), ['name' => 'newName', 'crop' => 'newCrop', 'coordinates' => '{}', 'area_id' => 1]);
+
+        $response->assertSessionHas('success');
+    }
+
+    /** @test **/
+    public function a_user_with_admin_role_can_edit_his_zone()
+    {
+        $response = $this->actingAs($this->userAdmin)
+            ->get(route('zone.edit', $this->zone));
+
+        $response->assertStatus(200);
+    }
+
+    /** @test **/
+    public function a_user_with_admin_role_can_update_his_zone()
+    {
+        $response = $this->actingAs($this->userAdmin)
+            ->put(route('zone.update', $this->zone), ['name' => 'newName', 'crop' => 'newCrop', 'coordinates' => '{}', 'area_id' => 1]);
+        $response->assertSessionHas('success');
+    }
+
+    /** @test **/
+    public function a_user_with_admin_role_can_delete_his_zone()
+    {
+        $response = $this->actingAs($this->userAdmin)
+            ->delete(route('zone.destroy', $this->zone));
+
+        $response->assertSessionHas('success');
+    }
+
+}
---
+++ tests/Feature/ZoneControllerViewerTest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Area;
+use App\Models\Role;
+use App\Models\SensorNode;
+use App\Models\User;
+use App\Models\Zone;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ZoneControllerViewerTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public $userAdmin;
+    public $userViewer;
+    public $area;
+    public $zone;
+    public $sensorNode;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // Create Admin role
+        factory(Role::class)->create();
+        // Create Viewer role
+        factory(Role::class)->create([
+            'name' => 'viewer',
+            'display_name' => 'Viewer',
+            'description' => 'Viewer'
+        ]);
+
+        $this->userAdmin = factory(User::class)->create();
+        $this->userAdmin->attachRole('admin');
+
+        $this->userViewer = factory(User::class)->create(['account_id' => $this->userAdmin->account_id]);
+        $this->userViewer->attachRole('viewer');
+
+        $this->area = factory(Area::class)->create(['account_id' => $this->userAdmin->account->id]);
+        $this->zone = factory(Zone::class)->create(['area_id' => $this->area->id]);
+        $this->sensorNode = factory(SensorNode::class)->create(['zone_id' => $this->zone->id, 'account_id' => $this->userAdmin->id]);
+    }
+
+    /** @test **/
+    public function a_user_with_viewer_role_can_index_his_zone()
+    {
+        $response = $this->actingAs($this->userViewer)
+            ->get(route('zone.index'));
+
+        $response->assertStatus(200);
+    }
+
+    /** @test **/
+    public function a_user_with_viewer_role_can_show_his_zone()
+    {
+        $response = $this->actingAs($this->userViewer)
+            ->get(route('zone.show', $this->zone));
+
+        $response->assertStatus(200);
+    }
+
+    /** @test **/
+    public function a_user_with_viewer_role_cannot_create_his_area()
+    {
+        $response = $this->actingAs($this->userViewer)
+            ->get(route('zone.create'));
+
+        $response->assertSessionHas('warning');
+    }
+
+    /** @test **/
+    public function a_user_with_viewer_role_cannot_store_his_area()
+    {
+        $response = $this->actingAs($this->userViewer)
+            ->post(route('zone.store', $this->zone), ['name' => 'newName', 'crop' => 'newCrop', 'coordinates' => '{}', 'area_id' => 1]);
+
+        $response->assertSessionHas('warning');
+    }
+
+    /** @test **/
+    public function a_user_with_viewer_role_cannot_edit_his_zone()
+    {
+        $response = $this->actingAs($this->userViewer)
+            ->get(route('zone.edit', $this->zone));
+
+        $response->assertSessionHas('warning');
+    }
+
+    /** @test **/
+    public function a_user_with_viewer_role_cannot_update_his_zone()
+    {
+        $response = $this->actingAs($this->userViewer)
+            ->put(route('zone.update', $this->zone), ['name' => 'newName', 'crop' => 'newCrop', 'coordinates' => '{}', 'area_id' => 1]);
+        $response->assertSessionHas('warning');
+    }
+
+    /** @test **/
+    public function a_user_with_viewer_role_cannot_delete_his_zone()
+    {
+        $response = $this->actingAs($this->userViewer)
+            ->delete(route('zone.destroy', $this->zone));
+
+        $response->assertSessionHas('warning');
+    }
+
+}
