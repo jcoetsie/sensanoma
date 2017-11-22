8c82802f927754ef265368702730c2622e76c15b
hxn-dev <hxn@live.fr>
2017-11-22 16:59:01 +0100

    Update influxdb config

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          * Package Service Providers...
          */
         Spatie\Html\HtmlServiceProvider::class,
+        TrayLabs\InfluxDB\Providers\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Html' => Spatie\Html\Facades\Html::class,
+        'InfluxDB' => TrayLabs\InfluxDB\Facades\InfluxDB::class,
     ],
 
 ];
