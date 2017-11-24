1ae8928505103b204697073e99d6925f7ec468ba
hxn-dev <hxn@live.fr>
2017-11-24 21:51:41 +0100

    Add grafana to after.sh

--- after.sh
+++ after.sh
@@ -4,8 +4,15 @@
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
 
+# InfluxDB
 curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 source /etc/lsb-release
 echo "deb https://repos.influxdata.com/ubuntu xenial stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 sudo apt-get update && sudo apt-get install influxdb
 sudo service influxdb start
+
+# Grafana
+wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_4.6.2_amd64.deb
+sudo apt-get install -y adduser libfontconfig
+sudo dpkg -i grafana_4.6.2_amd64.deb
+sudo service grafana-server start
