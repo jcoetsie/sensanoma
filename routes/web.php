8be1525862e7ce50c54278e9961c0553c3d6eaa2
DYSurroundings <joao_mnstrkhaos@hotmail.com>
2017-11-27 16:09:40 +0100

    Update user can no longer access admin pages or view them

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,10 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::group(['middleware' => 'auth'], function () {
 
     Route::group(['middleware' => 'role:admin'], function() {
-        Route::resource('account', 'AccountController')->except('index', 'show');
-        Route::resource('area', 'AreaController')->except('index', 'show');
-        Route::resource('zone', 'ZoneController')->except('index', 'show');
-        Route::resource('sensor_node', 'SensorNodeController')->except('index', 'show');
-    });
-
-    Route::group(['middleware' => 'role:viewer|admin'], function() {
-        Route::resource('account', 'AccountController')->only('index', 'show');
-        Route::resource('area', 'AreaController')->only('index', 'show');
-        Route::resource('zone', 'ZoneController')->only('index', 'show');
-        Route::resource('sensor_node', 'SensorNodeController')->only('index', 'show');
+        Route::resource('account', 'AccountController');
+        Route::resource('area', 'AreaController');
+        Route::resource('zone', 'ZoneController');
+        Route::resource('sensor_node', 'SensorNodeController');
     });
 
     Route::get('user', 'UserController@profile');
