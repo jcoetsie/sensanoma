700e942b31f544066bfe8714ebb43a8966e7381f
hxn-dev <hxn@live.fr>
2017-12-07 12:23:07 +0100

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,12 @@ Route::group(['middleware' => 'auth'], function () {
         Route::resource('area', 'AreaController');
         Route::resource('zone', 'ZoneController');
         Route::resource('sensor_node', 'SensorNodeController');
-        Route::delete('user', 'UserController@destroy')->name('user.destroy');
-        Route::delete('user{id}', 'UserController@viewerDestroy')->name('viewer.destroy');
+        Route::delete('user/account/{user}', 'UserController@destroy')->name('user.destroy');
+        Route::delete('user/{user}', 'UserController@viewerDestroy')->name('viewer.destroy');
     });
 
-    Route::get('user', 'UserController@profile');
-    Route::put('user' , 'UserController@update');
-    // Route::delete('profile/{id}' , 'UserController@destroy');
-
-
-
+    Route::get('user', 'UserController@profile')->name('user.profile');
+    Route::put('user' , 'UserController@update')->name('user.profile.update');
 });
 
 Auth::routes();
